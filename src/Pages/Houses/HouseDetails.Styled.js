35e6782efd36796010f45cdbf95eeb8adcb2a32c
jepwebbe <jepwebbe@gmail.com>
2023-02-15 11:58:15 +0100

    houseDetails map & fetch almost done

--- src/Pages/Houses/HouseDetails.Styled.js
+++ src/Pages/Houses/HouseDetails.Styled.js
@@ -2,45 +2,68 @@ import styled from "styled-components";
 
 export const StyledHouseDetails = styled.section`
   margin-top: -8rem;
-  width: 100%;
   > div:nth-child(1) {
     grid-area: a;
-    aspect-ratio: 21/5;
+    aspect-ratio: 21/7;
     overflow: hidden;
   }
   div.detailsgrid {
+    padding: 0 ${(props) => props.theme.space.sidePadding};
     grid-area: b;
     display: grid;
     width: 100%;
+    margin-top: -1.5rem;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: auto auto 1fr;
-    grid-template-areas: "a b c" "d e f" "g e h";
-    div {
-      display: flex;
+    grid-template-areas: "a b c" "d e f" "g g h";
+    row-gap: 1rem;
+    >div {
+        padding: 0 1rem;
+        p, span {
+            margin: 0.3rem 0;
+        }
     }
     > div:nth-child(1) {
       grid-area: a;
+      background-color: rgba(255, 255, 255, 0.7);
     }
     > div:nth-child(2) {
       grid-area: b;
+      background-color: rgba(255, 255, 255, 0.7);
     }
     > div:nth-child(3) {
       grid-area: c;
+      background-color: rgba(255, 255, 255, 0.7);
+      text-align: right;
+      p.price {
+        span {
+          font-size: 1.5rem;
+        }
+      }
     }
     > div:nth-child(4) {
       grid-area: d;
+      display: grid;
+      grid-template-columns: 3fr 1fr;
     }
     > div:nth-child(5) {
       grid-area: e;
+      display: grid;
+      grid-template-columns: 3fr 1fr;
     }
     > div:nth-child(6) {
       grid-area: f;
+      display: grid;
+      grid-template-columns: 3fr 1fr;
     }
     > div:nth-child(7) {
       grid-area: g;
     }
     > div:nth-child(8) {
       grid-area: h;
+      h3 {
+        margin: 0;
+      }
     }
   }
 `;
--- src/Pages/Houses/HousesDetails.jsx
+++ src/Pages/Houses/HousesDetails.jsx
@@ -30,14 +30,83 @@ const HousesDetails = () => {
           />
         </div>
         <div className="detailsgrid">
-          <div>a</div>
+          <div>
+            <h2>{houseDetails?.address}</h2>
+            <p>
+              {houseDetails?.zipcode} {houseDetails?.city}
+            </p>
+            <p>
+              {houseDetails?.type} | {houseDetails.floo_space} |{" "}
+              {houseDetails?.num_rooms}
+            </p>
+          </div>
           <div>b</div>
-          <div>c</div>
-          <div>d</div>
-          <div>e</div>
-          <div>f</div>
-          <div>g</div>
-          <div>h</div>
+          <div>
+            <p className="price">
+              Kontantpris <span>{houseDetails?.price}</span>
+            </p>
+            <p>Udbetaling {houseDetails?.payout}</p>
+            <p>Ejerudgift pr måned {houseDetails?.cost}</p>
+          </div>
+          <div>
+            <p>Sagsnr.</p>
+            <span>{houseDetails?.id}</span>
+            <p>Boligareal</p>
+            <span>{houseDetails?.floor_space}</span>
+            <p>Grundareal</p>
+            <span>{houseDetails?.ground_space}</span>
+            <p>Antal rum</p>
+            <span>{houseDetails?.num_rooms}</span>
+            <p>Antal plan</p>
+            <span>{houseDetails?.num_floors}</span>
+          </div>
+          <div>
+            <p>Kælder</p>
+            <span>{houseDetails?.floor_space}</span>
+            <p>Byggeår</p>
+            <span>{houseDetails?.ground_space}</span>
+            <p>Ombygget</p>
+            <span>{houseDetails?.num_rooms}</span>
+            <p>Energimærke</p>
+            <span>{houseDetails?.num_floors}</span>
+            <p>Liggetid</p>
+            <span>{houseDetails?.id}</span>
+          </div>
+          <div>
+            <p>Kontantpris</p>
+            <span>{houseDetails?.price}</span>
+            <p>Udbetaling</p>
+            <span>{houseDetails?.ground_space}</span>
+            <p>Brutto ex. ejerudgift</p>
+            <span>{houseDetails?.num_rooms}</span>
+            <p>Netto ex. ejerudgift</p>
+            <span>{houseDetails?.num_floors}</span>
+            <p>Ejerudgift</p>
+            <span>{houseDetails?.id}</span>
+          </div>
+          <div>{houseDetails?.description}</div>
+          <div>
+            <article>
+                <h3>Kontakt</h3>
+              <img
+                src={houseDetails?.staff?.image}
+                alt={`Et billede af ${houseDetails?.staff?.firstname}`}
+              />
+              <div>
+                <h3>
+                  {houseDetails?.staff?.firstname}{" "}
+                  {houseDetails?.staff?.lastname}
+                </h3>
+                <p>{houseDetails?.staff?.position}</p>
+                <p>
+                  Telefonnummer {houseDetails?.staff?.phone} Email:
+                  <a href={`mailto:${houseDetails?.staff?.email}`}>
+                    {houseDetails?.staff?.email}
+                  </a>
+                </p>
+              </div>
+            </article>
+          </div>
         </div>
       </StyledHouseDetails>
     </PageThree>
